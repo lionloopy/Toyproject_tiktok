d90af97cf1ab3f01cb5a93dd0f8ff5d933ffdb15
boompeak <wjfekr1@naver.com>
2023-01-10 17:16:49 +0900

    회원가입_승호입니다.

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def music_get():
     return jsonify({'musics':music_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5300, debug=True)
