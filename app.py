b9d5c76cb5a07a40c6814aa30f2056697444b5e5
DESKTOP-TVVL0QD\user <potenter11@naver.com>
2023-01-10 15:37:14 +0900

    [황원준] - app.py 서버 통합, 페이지 연결 진행중!!

--- app.py
+++ app.py
@@ -1,19 +1,87 @@
-from flask import Flask, render_template, request, jsonify
+import jwt
+import datetime
+import hashlib
+import certifi
 
-<<<<<<< HEAD
-print("커밋한다")
-print("test입니다. 승호")
-print('test입니다-원준')
-print('test입니다-윤서')
-=======
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+app = Flask(__name__)
+
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.fgag4po.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
->>>>>>> develope
 
 # from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+from datetime import datetime, timedelta
+
+from pymongo import MongoClient
+import certifi
+
+# 로그인
+SECERY_KEY = 'SPARTA'
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECERY_KEY, algorithms=['HS256'])
+        id = payload['id']
+        return render_template('index.html', username=id)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for('login',msg='로그인 시간이 만료되었습니다.'))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for('login',msg='로그인 정보가 존재하지 않습니다.'))
+
+@app.route('/login')
+def login():
+    msg = request.args.get('msg')
+    return render_template('index.html', msg=msg)
+@app.route("/sign_in", methods=["POST"])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username':username_receive,'password':pw_hash})
+
+    if result is not None:
+        payload = {
+            'id':username_receive,
+            'exp': datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECERY_KEY, algorithm='HS256')
+        return jsonify({'result':'success','token':token, 'msg':'로그인 성공!'})
+    else:
+        return jsonify({'result': 'fail','msg':'아이디/비밀번호가 일치하지 않습니다.'})
+
+# 회원가입
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://seungho:19960724@cluster0.y2z4pif.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup_index.html')
+
+@app.route("/api/signup", methods=["POST"])
+def web_signup_post():
+    name_receive = request.form['name_give']
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+    doc = {
+        'name':name_receive,
+        'email':email_receive,
+        'password':password_receive
+    }
+    db.signup.insert_one(doc)
+
+    return jsonify({'msg': '가입 완료'})
+
+@app.route("/signin", methods=["GET"])
+def web_signup_get():
+    user_list = list(db.signup.find({}, {'_id': False}))
+    return jsonify({'users': user_list})
 
 @app.route('/playList')
 def posting():
