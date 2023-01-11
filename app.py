0dea452aef5b9baea9fb47cab1fb7ab27d3bcf77
DESKTOP-TVVL0QD\user <potenter11@naver.com>
2023-01-11 14:44:30 +0900

    [황원준] - merge전 확인

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
-import jwt, datetime, hashlib, certifi
+from bson import ObjectId
+import jwt
+import datetime
+import hashlib
 
-from flask import Flask, render_template, request, jsonify, url_for, redirect
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://yunseo:sparta@cluster0.6bemlvq.mongodb.net/?retryWrites=true&w=majority')
+import certifi
+
+from bs4 import BeautifulSoup
+
+##### 회원가입
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://yunseo:sparta@cluster0.6bemlvq.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
-# from bs4 import BeautifulSoup
+@app.route('/signup')
+def sign_up():
+    return render_template('signup_index.html')
+
+@app.route("/api/signup", methods=["POST"])
+def web_signup_post():
+    name_receive = request.form['name_give']
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        'name': name_receive,
+        'email': email_receive,
+        'password': password_hash
+    }
+    db.users.insert_one(doc)
+
+    return jsonify({'msg': '가입 완료'})
+
+@app.route("/signin", methods=["GET"])
+def web_signup_get():
+    user_list = list(db.signup.find({}, {'_id': False}))
+    return jsonify({'users': user_list})
 
 ##### 로그인
 SECERY_KEY = 'SPARTA'
@@ -32,7 +67,7 @@ def login():
 @app.route("/api/sign_in", methods=["POST"])
 def sign_in():
     email_receive = request.form['email_give']
-    password_receive = request.form['password_give']
+    password_receive = request.form['pw_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     result = db.users.find_one({'email': email_receive, 'password': pw_hash})
@@ -40,43 +75,21 @@ def sign_in():
     if result is not None:
         payload = {
             'id': email_receive,
-            'exp': datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
         }
         token = jwt.encode(payload, SECERY_KEY, algorithm='HS256')
         return jsonify({'result': 'success','token': token, 'msg': '로그인 성공!'})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디 또는 비밀번호가 일치하지 않습니다.'})
 
-##### 회원가입
-ca = certifi.where()
+#### 상세페이지
 
-client = MongoClient('mongodb+srv://yunseo:sparta@cluster0.6bemlvq.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
-db = client.dbsparta
+@app.route("/posting", methods=["GET"])
+def posting_get():
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
-@app.route('/signup')
-def sign_up():
-    return render_template('signup_index.html')
-
-@app.route("/api/signup", methods=["POST"])
-def web_signup_post():
-    name_receive = request.form['name_give']
-    email_receive = request.form['email_give']
-    password_receive = request.form['password_give']
-    doc = {
-        'name': name_receive,
-        'email': email_receive,
-        'password': password_receive
-    }
-    db.users.insert_one(doc)
-
-    return jsonify({'msg': '가입 완료'})
-
-@app.route("/signin", methods=["GET"])
-def web_signup_get():
-    user_list = list(db.signup.find({}, {'_id': False}))
-    return jsonify({'users': user_list})
-
-@app.route('/playList')
+@app.route('/page/detail')
 def posting():
    return render_template('posting.html')
 
@@ -95,17 +108,37 @@ def comment_post():
 
     return jsonify({'msg': '저장되었습니다!'})
 
-@app.route("/posting", methods=["GET"])
-def posting_get():
-    comment_list = list(db.comments.find({}, {'_id': False}))
-    return jsonify({'comments': comment_list})
+####
+@app.route("/comment/delete", methods=["POST"])
+def comment_delete():
 
-# url = 'https://www.melon.com/landing/playList.htm?type=djc&plylstTypeCode=M20002&plylstSeq=453051002'
+    number_receive = request.form['number_give']
+    comments_receive = request.form['comments_give']
+
+    doc = {
+        'num' : number_receive,
+        'comment' : comments_receive
+    }
+
+    db.comments.delete_one(doc)
+
+    return jsonify({'msg': '삭제되었습니다!'})
+
+
+# @app.route("/posting/image", methods=["GET"])
+# def posting_music():
+#     url = 'https://www.melon.com/landing/playList.htm?type=djc&plylstTypeCode=M20002&plylstSeq=453051002'
 #
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# data = requests.get(url, headers=headers)
+#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+#     data = requests.get(url, headers=headers)
 #
-# soup = BeautifulSoup(data.text, 'html.parser')
+#     soup = BeautifulSoup(data.text, 'html.parser')
+#     # frm > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > a > img
+#     # frm > div > table > tbody > tr:nth-child(4) > td:nth-child(3) > div > a > img
+#     images = soup.select('div > table > tbody > tr')
+#     for img in images:
+#         b = img.select_one('td > div > a > img')
+#         print(b['src'])
 
 # frm > div > table > tbody > tr:nth-child(4) > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a
 # frm > div > table > tbody > tr:nth-child(5) > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a
@@ -116,12 +149,7 @@ def posting_get():
 #     if a is not None:
 #         print(a.text)
 
-# frm > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > a > img
-# frm > div > table > tbody > tr:nth-child(4) > td:nth-child(3) > div > a > img
-# images = soup.select('div > table > tbody > tr')
-# for img in images:
-#     b = img.select_one('td > div > a > img')
-#     print(b['src'])
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
