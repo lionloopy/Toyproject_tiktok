a827ba4c94a224152e999e3b051c9f0859e579a0
DESKTOP-TVVL0QD\user <potenter11@naver.com>
2023-01-11 19:49:07 +0900

    메인페이지와 상세페이지 페이지만 연결

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from bson import ObjectId
 import jwt
 import datetime
 import hashlib
+import requests
 
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from datetime import datetime, timedelta
@@ -16,7 +17,7 @@ from bs4 import BeautifulSoup
 ##### 회원가입
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://yunseo:sparta@cluster0.6bemlvq.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.fgag4po.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/signup')
@@ -82,16 +83,57 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디 또는 비밀번호가 일치하지 않습니다.'})
 
+#### 메인페이지
+@app.route('/page/main')
+def main():
+    return render_template('main_posting.html')
+
+@app.route("/music", methods=["POST"])
+def music_post():
+    url_receive = request.form['url_give']
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.melon.com/landing/playList.htm?type=djc&plylstTypeCode=M20002&plylstSeq=453051002', headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+images = soup.select_one('div > table > tbody > tr')
+
+a = soup.select('#frm > div > table > tbody > tr')
+
+for music in a:
+    for img in images:
+        b = music.select_one('div > a > img')
+    rank = music.select_one('div > span.rank').text[0:2].strip()
+    title = music.select_one('div > div > div.ellipsis.rank01 > span > a').text.strip()
+    singer = music.select_one('div > div > div.ellipsis.rank02 > a').text
+    album = music.select_one('td:nth-child(6) > div > div > div > a').text
+
+    if music is not None:
+        doc = {
+            'image': b['src'],
+            'rank': rank,
+            'title': title,
+            'singer': singer,
+            'album': album
+        }
+    db.musics.insert_one(doc)
+@app.route("/music", methods=["GET"])
+def music_get():
+    music_list = list(db.musics.find({}, {'_id': False}))
+    return jsonify({'musics': music_list})
+
 #### 상세페이지
-
-@app.route("/posting", methods=["GET"])
-def posting_get():
-    comment_list = list(db.comments.find({}, {'_id': False}))
-    return jsonify({'comments': comment_list})
-
 @app.route('/page/detail')
 def posting():
    return render_template('posting.html')
+
+@app.route("/posting", methods=["GET"])
+def posting_get():
+    music_list = list(db.musics.find({}, {'_id': False}))
+    # comment_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'musics': music_list})
+
 
 @app.route("/posting", methods=["POST"])
 def comment_post():
@@ -108,8 +150,7 @@ def comment_post():
 
     return jsonify({'msg': '저장되었습니다!'})
 
-####
-@app.route("/comment/delete", methods=["POST"])
+@app.route("/posting/delete", methods=["POST"])
 def comment_delete():
 
     number_receive = request.form['number_give']
@@ -123,32 +164,6 @@ def comment_delete():
     db.comments.delete_one(doc)
 
     return jsonify({'msg': '삭제되었습니다!'})
-
-
-# @app.route("/posting/image", methods=["GET"])
-# def posting_music():
-#     url = 'https://www.melon.com/landing/playList.htm?type=djc&plylstTypeCode=M20002&plylstSeq=453051002'
-#
-#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get(url, headers=headers)
-#
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#     # frm > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > a > img
-#     # frm > div > table > tbody > tr:nth-child(4) > td:nth-child(3) > div > a > img
-#     images = soup.select('div > table > tbody > tr')
-#     for img in images:
-#         b = img.select_one('td > div > a > img')
-#         print(b['src'])
-
-# frm > div > table > tbody > tr:nth-child(4) > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a
-# frm > div > table > tbody > tr:nth-child(5) > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a
-
-# musics = soup.select('div > table > tbody > tr')
-# for music in musics:
-#     a = music.select_one('td > div > div > div.ellipsis.rank01 > span > a')
-#     if a is not None:
-#         print(a.text)
-
 
 
 if __name__ == '__main__':
