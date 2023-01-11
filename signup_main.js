5ac22eefa2b7fbbad6d418c8d2f67f0826240fbf
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-11 17:24:25 +0900

    Add files via upload

--- signup_main.js
+++ signup_main.js
@@ -60,6 +60,12 @@ function save_account() {
         }
     }
 
+    // 약관 동의 제어
+    if(!cAll.checked){
+        alert("약관에 동의하세요")
+        return;
+    }
+
 
     // 회원가입 정보 포스트
     $.ajax({
@@ -79,9 +85,66 @@ const getActive = (target) => {
     target.classList.add('active')
 }
 let fieldList = document.querySelectorAll("input")
-// fieldList.splice(4,0,"ddd")
 fieldList.forEach(field => field.addEventListener('focus', (e) => {
     getActive(e.target)
 }))
-// document.getElementById("#selectEmail").addEventListener("click",(e)=>alert('d'))
-console.log(document.getElementById("#selectEmail"))
+
+// Welcome UI
+const showName = () => {
+    $('#welcome-area').empty()
+    $('#welcome-area').append('<h8 style="color: white">.</h8>')
+    let firstName = $('#firstName').val()
+    let lastName = $('#lastName').val()
+    if (firstName === '' || lastName === '') {
+        return
+    }
+    let fullName = lastName + firstName
+    let tmp_html = `<h8 id="welcome-name">${fullName}님 반갑습니다.</h8>`
+    $('#welcome-area').append(tmp_html)
+    $('#welcome-name').fadeIn(2000)
+
+}
+fieldList[1].addEventListener("blur", () => {
+    showName()
+})
+fieldList[0].addEventListener("blur", () => {
+    showName()
+})
+
+//팝업 관련
+var target = document.querySelector('.btn_open');
+var btnPopClose = document.querySelector('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+target.addEventListener('click', function () {
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.display = 'block';
+});
+
+// 팝업 닫기
+btnPopClose.addEventListener('click', function () {
+    this.parentNode.parentNode.style.display = 'none';
+});
+
+// 팝업 체크
+const c1 = document.getElementById("c1")
+const c2 = document.getElementById("c2")
+const c3 = document.getElementById("c3")
+const cAll = document.getElementById("c_all")
+const checkArea = document.getElementById("check-area")
+
+//전체 체크
+cAll.addEventListener("change", () => {
+    c1.checked = cAll.checked
+    c2.checked = cAll.checked
+    c3.checked = cAll.checked
+})
+checkArea.addEventListener("change", () => {
+    if (c1.checked && c2.checked && c3.checked) {
+        cAll.checked = true
+    }
+    if (cAll.checked) {
+        btnPopClose.click()
+    }
+})
