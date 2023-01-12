3fb9923a94c04f547ecd5b86cb9cb66a18a0ade3
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-12 16:02:47 +0900

    Add files via upload

--- static/titleList.js
+++ static/titleList.js
@@ -1,38 +1,45 @@
+$(document).ready(function () {
+    listing();
+});
 
-        $(document).ready(function () {
-            listing();
-        });
+function listing() {
+    $.ajax({
+        type: 'GET',
+        url: '/music',
+        data: {},
+        success: function (response) {
+            let rows = response['musics']
 
-        function listing() {
-            $.ajax({
-                type: 'GET',
-                url: '/music',
-                data: {},
-                success: function (response) {
-                    let rows = response['musics']
+            for (let i = 0; i < 50; i++) {
+                let rank = rows[i]['rank'];
+                let image = rows[i]['image'];
+                let title = rows[i]['title'];
+                let singer = rows[i]['singer'];
+                let album = rows[i]['album'];
 
-                    for (let i = 0; i < 50; i++) {
-                        let rank = rows[i]['rank']
-                        let image = rows[i]['image']
-                        let title = rows[i]['title']
-                        let singer = rows[i]['singer']
-                        let album = rows[i]['album']
-
-                            let temp_html = `<tr id="table" class="chart-text" onclick="show_detail(${rank})">
+                let temp_html = `<tr id="table" class="chart-text" onclick="moveToPostingPage(${rank})">
                                                 <th scope="col" id="num" style="width: 80px;">${rank}</th>
                                                 <th id="image" scope="col" colspan="3" style="width: 25px;height: 25px;"><img src="${image}"></th>
                                                 <th scope="col" id="title" style="width: 200px">${title}</th>
                                                 <th id="singer" scope="col" style="width: 50px">${singer}</th>
                                                 <th id="album" scope="col" class="album-text">${album}</th>
-                                            </tr>`
-                        $('#music-box').append(temp_html)
-                    }
-                }
-
-            })
-        };
-
-        function show_detail(rank){
-            alert('안녕!')
-            window.location.href = '/page/detail'
+                                         </tr>`
+                $('#music-box').append(temp_html)
+            }
         }
+    })
+}
+
+const moveToPostingPage = (rank) => {
+    // let a = new URL('http://127.0.0.1:5100/page/detail')
+    // let b = new URLSearchParams(a)
+    // let c = b.set("rank",rank)
+    // window.location.href = c
+    // console.log(c)
+
+    let a = new URL(`http://localhost:5000/page/detail?rank=${rank}`)
+
+    window.location.href = a
+
+// ?gil=yes&log=wow&gillog=good
+}
